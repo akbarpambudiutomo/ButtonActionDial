21a573f41517f66f725ac1e33750a890cc729632
akbarpambudiutomo <akbarpambudi31@gmail.com>
2019-03-05 08:51:10 +0700

    commit all

---
+++ app/src/main/java/com/example/akbar/buttonactiondial/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.akbar.buttonactiondial;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    String phone = "(021) - 7546758";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.btn_call);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + phone));
+                startActivity(intent);
+            }
+        });
+
+    }
+}
